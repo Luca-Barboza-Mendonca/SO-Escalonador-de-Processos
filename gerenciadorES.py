46ea7156c7150d02723be6cb00d987e7dd4f6214
Luca-Barboza-Mendonca <lucabarbozamendonca@gmail.com>
2023-08-31 16:48:01 -0300

    thread dispositivo

--- gerenciadorES.py
+++ gerenciadorES.py
@@ -1,8 +1,26 @@
+import typing
+from PyQt5.QtCore import QObject
 from escalonador import *
+import threading
 
 # numDispositivos = número de threads que devem ser incializadas
 
-def initDispositivos():
-    # Incializar threads de dispositivo
-    pass
+dispositivos = []
 
+def initDispositivo(id, numSimultaneos, tempoOperacao):
+    # Incializar thread de dispositivo
+
+    global dispositivos
+    
+    for i in range(0, numDispositivos):
+        thread = Dispositivo(id, numSimultaneos, tempoOperacao)
+        dispositivos.append(thread)
+
+class Dispositivo(QThread):
+
+    text_device = pyqtSignal(str)
+
+    def __init__(self, id, numSimultaneos, tempoOperacao):
+        self.id = id
+        self.numSimultaneos = numSimultaneos
+        self.tempoOperacao = tempoOperacao
